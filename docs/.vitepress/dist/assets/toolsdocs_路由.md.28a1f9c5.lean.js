ed342013d462a8899ffb007d33ad374c90326d22
Leaond <1503600235@qq.com>
2023-10-13 10:29:57 +0800

    upload config.ts

---
+++ docs/.vitepress/dist/assets/toolsdocs_路由.md.28a1f9c5.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as e,c as a,Q as n}from"./chunks/framework.37d083f2.js";const h=JSON.parse('{"title":"路由","description":"","frontmatter":{},"headers":[],"relativePath":"toolsdocs/路由.md","filePath":"toolsdocs/路由.md"}'),l={name:"toolsdocs/路由.md"},p=n("",6),t=[p];function o(c,i,d,r,g,m){return e(),a("div",null,t)}const _=s(l,[["render",o]]);export{h as __pageData,_ as default};
